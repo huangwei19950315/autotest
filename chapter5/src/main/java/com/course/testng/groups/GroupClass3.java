7fe61f2b10acbdd019f09a0155134584312dca7e
黄伟 <emotibot@huangweideMacBook-Pro.local>
2019-12-11 14:45:45 +0800

    12.11

--- chapter5/src/main/java/com/course/testng/groups/GroupClass3.java
+++ chapter5/src/main/java/com/course/testng/groups/GroupClass3.java
@@ -2,10 +2,14 @@ package com.course.testng.groups;
 
 import org.testng.annotations.Test;
 
-@Test
+@Test(groups = "tea")
 public class GroupClass3 {
 
     public void tea1(){
         System.out.println("这是group3的tea1");
+    }
+
+    public void tea2(){
+        System.out.println("这是group3的tea2");
     }
 }
